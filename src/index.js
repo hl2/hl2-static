a02d649729cccd23acbcfb2f43dc952f6600e7f7
Guillaume <grrseguin@gmail.com>
2016-08-11 08:43:10 +0200

    feat (images) social network
    > for first email

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 require('./images/logo_lt_600.png');
+require('./images/socialnetwork_facebook_48.png');
+require('./images/socialnetwork_link_48.png');
+require('./images/socialnetwork_twitter_48.png');
