b5426689b32bb54513978f72eaad4c4554d873d2
Kevin Pollet <pollet.kevin@gmail.com>
2017-01-13 09:15:35 +0100

    Add hl2 panorama horizontal logo to email media

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require('./images/email/hl2-panorama-logo-longeur.png');
+require('./images/email/hl2-panorama-horizontal-logo.png');
 require('./images/email/social-link-facebook.png');
 require('./images/email/social-link-hl2.png');
 require('./images/email/social-link-twitter.png');
