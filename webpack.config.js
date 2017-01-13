db91abaf72e0d5c628114f3a89e51c2894570eb6
Kevin Pollet <pollet.kevin@gmail.com>
2017-01-13 10:11:40 +0100

    Use current file name and path in dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var webpack = require('webpack');
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
     entry: path.join(__dirname, 'src'),
@@ -11,7 +11,7 @@ module.exports = {
         loaders: [{
             test: /\.(jpe?g|png|gif|svg)$/i,
             loaders: [
-                'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                'file?name=[path][name].[ext]&context=src',
                 'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
             ]
         }]
