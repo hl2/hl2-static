4b9b49aab9453aaeb1a4c2240d8ea8393bbb4a8c
Kevin Pollet <pollet.kevin@gmail.com>
2017-01-13 10:10:49 +0100

    Add image size to file names

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require('./images/email/hl2-panorama-horizontal-logo.png');
-require('./images/email/social-link-facebook.png');
-require('./images/email/social-link-hl2.png');
-require('./images/email/social-link-twitter.png');
+require('./images/email/social-link-hl2-48.png');
+require('./images/email/social-link-twitter-48.png');
+require('./images/email/social-link-facebook-48.png');
+require('./images/email/hl2-panorama-horizontal-logo-300-78.png');
