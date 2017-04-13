bd023ee68ca92199fad693efe24724d5e4d6f236
Guillaume <grrseguin@gmail.com>
2017-04-13 13:37:50 +0200

    Upgrade webpack to version 2
    > List all image in an html page
    > Add local serve
    > Show a default HTML page
    > use exact npm version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
-require('./images/email/social-link-hl2-48.png');
-require('./images/email/social-link-twitter-48.png');
-require('./images/email/social-link-facebook-48.png');
-require('./images/email/hl2-panorama-horizontal-logo-600-157.png');
+require('./index.html');
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,51 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    devtool: 'cheap-eval-source-map',
     entry: path.join(__dirname, 'src'),
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'index.js'
     },
     module: {
-        loaders: [{
-            test: /\.(jpe?g|png|gif|svg)$/i,
-            loaders: [
-                'file?name=[path][name].[ext]&context=src',
-                'image-webpack?optimizationLevel=7&interlaced=false'
-            ]
-        }]
-    }
+        rules: [
+
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            context: 'src',
+                            name: '[path][name].[ext]'
+                        }
+                    },
+                    {
+                      loader: 'image-webpack-loader',
+                      options: {
+                        optimizationLevel: 7,
+                        interlaced: false,
+                      },
+                    },
+                ]
+            },
+            {
+                test: /\.html$/i,
+                use: [
+
+                  {
+                    loader: 'html-loader',
+                  },
+                ]
+            },
+        ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+      }),
+    ],
 };
